3ee8fe6e410d7ff223e163c273224c9b3bc57a88
quazamuhaha <quazamuhaha@hotmail.com>
2015-08-13 19:48:47 +1000

    Master
    Update

---
+++ Form.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!--?php
+    $Name = $_POST[Name];
+    $Email = $_POST[Email];
+    $textarea-a = $_POST[textarea-a];
+?-->
+</body>
+</html>
